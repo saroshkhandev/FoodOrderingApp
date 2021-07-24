57c460f252c56a24e51b33ad9bdf15541fe15619
Sarosh <saroshabdullah2013@gmail.com>
2021-07-25 00:09:24 +0530

    Added Button for Cart

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,15 +1,18 @@
 import {Fragment} from "react";
+
+import HeaderCartButton from "./HeaderCartButton"
 import classes from "./Header.module.css";
+import mealsImage from "../../assets/meals.jpg"
 
 const Header = (props) => {
     return (
         <Fragment>
-            <header>
+            <header className={classes.header}>
                 <h1>Amna Rasoi</h1>
-                <button>Cart</button>
+                <HeaderCartButton />
             </header>
-            <div>
-                <img />
+            <div className={classes["main-image"]}>
+                <img src={mealsImage} alt="A table full of delicious food" />
             </div>
         </Fragment>
     );
---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,8 @@
+
+const Button = (props) => {
+    return (
+        <button type={props.type}></button>
+    );
+};
+
+export default Button;
