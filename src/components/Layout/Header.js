69bdde347f8422451248b1bffc43b7866738ff59
Sarosh <saroshabdullah2013@gmail.com>
2021-07-25 02:31:51 +0530

    added add to cart functionality

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
     return (
         <Fragment>
             <header className={classes.header}>
-                <h1>Amna Rasoi</h1>
+                <h1>Food Pony!</h1>
                 <HeaderCartButton onClick={props.onShowCart} />
             </header>
             <div className={classes["main-image"]}>
