e99b516be9146dbdc6ffe1198be91b52d1e2eff6
Sarosh <saroshabdullah2013@gmail.com>
2021-07-25 01:37:35 +0530

    Added Overlay close methods

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -9,7 +9,7 @@ const Header = (props) => {
         <Fragment>
             <header className={classes.header}>
                 <h1>Amna Rasoi</h1>
-                <HeaderCartButton />
+                <HeaderCartButton onClick={props.onShowCart} />
             </header>
             <div className={classes["main-image"]}>
                 <img src={mealsImage} alt="A table full of delicious food" />
