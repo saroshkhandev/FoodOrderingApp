732b9ba550cf256859873fa0a21127064ade08f9
Sarosh <saroshabdullah2013@gmail.com>
2021-07-26 15:43:33 +0530

    ADD REMOVE ITEM CART

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,5 +1,4 @@
 import { useReducer } from "react";
-
 import CartContext from "./cart-context";
 
 const defaultCartState = {
@@ -36,6 +35,27 @@ const cartReducer = (state, action) => {
     };
   }
   if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+
+    let updatedItems;
+
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
   }
 
   return defaultCartState;
