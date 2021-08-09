cf59bec7d9f397279c6b14212ddeea254c45665b
saroshkhandev <saroshabdullah2013@gmail.com>
2021-08-09 15:43:32 +0530

    Completely added checkout functionality with firebase

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -47,6 +47,13 @@ const Checkout = (props) => {
     if (!formIsValid) {
       return;
     }
+
+    props.onConfirm({
+      name: enteredName,
+      street: enteredStreet,
+      city: enteredCity,
+      postal: enteredPostal,
+    });
   };
   const nameControlClasses = `${classes.control} ${
     formInputsValidity.name ? "" : classes.invalid
